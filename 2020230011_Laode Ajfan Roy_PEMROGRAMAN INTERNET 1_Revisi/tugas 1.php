b50c2ed8cae0ed472659baf37979d6e5c1e0529a
Owsway <108323175+Owsway@users.noreply.github.com>
2023-01-24 00:44:30 +0700

    Add files via upload

---
+++ 2020230011_Laode Ajfan Roy_PEMROGRAMAN INTERNET 1_Revisi/tugas 1.php
@@ -0,0 +1,29 @@
+<?php
+echo "Nama : Rhesa Devara Widyaputra <br> ";
+echo "Tanggal Lahir : Jakarta, 18 November 2000 <br>";
+echo "Jenis Kelamin : Laki - Laki <br>";
+echo "Alamat : Vila Indah Permai Blok E5 No.3 <br>";
+echo "Agama : Islam <br>";
+echo "Status Perkawinan : Belum Kawin <br>";
+echo "Pekerjaan : Mahasiswa <br>";
+echo "Kewarganegaraan : WNI <br> <br> <br>";
+
+$nama = "Rhesa Devara Widyaputra";
+$tgl = "18 November 2000";
+$jk = "Laki - Laki";
+$alamat = "Vila Indah Permai Blok E5 No.3";
+$agama = "Islam";
+$status_p = "Belum Kawin";
+$pekerjaan = "Mahasiswa";
+$Kewarganegaraan = "WNI";
+
+
+echo "Nama : " ,$nama , "<br>";
+echo "Tanggal Lahir : " ,$tgl , "<br>";
+echo "Jenis Kelamin : " ,$jk , "<br>";
+echo "Alamat : " ,$alamat , "<br>";
+echo "Agama : " ,$agama , "<br>";
+echo "Status Perkawinan :" ,$status_p , "<br>";
+echo "Pekerjaan : " ,$pekerjaan , "<br>";
+echo "Kewarganegaraan : " ,$Kewarganegaraan , "<br>";
+?>
